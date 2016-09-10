3e74f3e783f6d109b87d0e6bbd82db80d19769c1
hiratsukashu <hiratsukashu@indival.co.jp>
2016-09-10 16:54:11 +0900

    Initial Commit

---
+++ LineCopy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LineCopy
+//
+//  Created by 平塚 俊輔 on 2016/09/10.
+//  Copyright © 2016年 平塚 俊輔. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LineCopy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LineCopy
+//
+//  Created by 平塚 俊輔 on 2016/09/10.
+//  Copyright © 2016年 平塚 俊輔. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
